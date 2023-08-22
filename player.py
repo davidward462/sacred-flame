cda35d68fc4cfc10b4bb3165b011ca5a793ca22e
David Ward <davidmward@mail.com>
2023-08-22 01:17:24 -0700

    add player sprite file

---
+++ player.py
@@ -0,0 +1,24 @@
+import pygame
+
+# class inherets from pygame sprite class
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+
+        # Initialize parent sprite class
+        super().__init__()
+
+        self.image = None
+        self.rect = None
+        
+    # Update sprite logic
+    def Update(self):
+        self.destroy()
+
+    # Destroy sprite
+    def Destroy(self):
+        condition = False
+        if condition:
+            self.kill()
+
+
+

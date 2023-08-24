0b8de4f67f3256b232335f81cf480fdfb6b92772
David Ward <davidmward@mail.com>
2023-08-24 14:11:04 -0700

    center rect and rename variables

--- player.py
+++ player.py
@@ -9,37 +9,38 @@ class Player(pygame.sprite.Sprite):
 
         # Image and rectangle
         self.image = pygame.image.load('graphics/player-temp.png')
-        self.rect = self.image.get_rect(midbottom = (startX,startY))
+        self.rect = self.image.get_rect()
+        self.rect.center = (startX, startY)
 
-        self.vx = 0
-        self.vy = 0
+        self.velocityX = 0
+        self.velocityY = 0
         self.speed = 6
 
         # Sounds
 
     def SetVelocity(self):
-        self.vx = 0
-        self.vy = 0
+        self.velocityX = 0
+        self.velocityY = 0
 
         # Get input and set velocity
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
-            self.vy -= 1
+            self.velocityY -= 1
         if keys[pygame.K_s]:
-            self.vy += 1
+            self.velocityY += 1
         if keys[pygame.K_a]:
-            self.vx -= 1
+            self.velocityX -= 1
         if keys[pygame.K_d]:
-            self.vx += 1
+            self.velocityX += 1
 
         # Diagonal
-        if self.vx != 0 and self.vy != 0:
-            self.vx *= 0.7071
-            self.vy *= 0.7071
+        if self.velocityX != 0 and self.velocityY != 0:
+            self.velocityX *= 0.7071
+            self.velocityY *= 0.7071
 
     def Move(self):
-        self.rect.x += (self.vx * self.speed)
-        self.rect.y += (self.vy * self.speed)
+        self.rect.x += (self.velocityX * self.speed)
+        self.rect.y += (self.velocityY * self.speed)
 
     # Update sprite logic
     def update(self):

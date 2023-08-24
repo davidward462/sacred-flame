e33d54b0096cd215cb802319c9bb21418de03fd7
David Ward <davidmward@mail.com>
2023-08-24 14:03:34 -0700

    move player on key input

--- player.py
+++ player.py
@@ -7,82 +7,45 @@ class Player(pygame.sprite.Sprite):
         # Initialize parent sprite class
         super().__init__()
 
-        # instance variables
-        self.moveUp = False
-        self.moveLeft = False
-        self.moveDown = False
-        self.moveRight = False
-
-        # Direction player should move
-        self.hDir = 0
-        self.vDir = 0
-
-        # Max speed of player in any direction
-        self.maxSpeed = 3.5
-
-        # Current speed
-        self.speed = 1
-
         # Image and rectangle
         self.image = pygame.image.load('graphics/player-temp.png')
         self.rect = self.image.get_rect(midbottom = (startX,startY))
 
+        self.vx = 0
+        self.vy = 0
+        self.speed = 3
+        self.direction =  [0, 0]
+
         # Sounds
 
-    def GetInput(self):
+    def SetVelocity(self):
+        self.vx = 0
+        self.vy = 0
+
+        # Get input and set velocity
         keys = pygame.key.get_pressed()
-        self.moveUp = keys[pygame.K_w]
-        self.moveLeft = keys[pygame.K_a]
-        self.moveDown = keys[pygame.K_s]
-        self.moveRight = keys[pygame.K_d]
+        if keys[pygame.K_w]:
+            self.vy -= 1
+        if keys[pygame.K_s]:
+            self.vy += 1
+        if keys[pygame.K_a]:
+            self.vx -= 1
+        if keys[pygame.K_d]:
+            self.vx += 1
 
-    def SetHorizontalDir(self):
-        hDir = 0
-        if self.moveLeft:
-            hDir -= 1
-        if self.moveRight:
-            hDir += 1
+        # Diagonal
+        if self.vx != 0 and self.vy != 0:
+            self.vx *= 0.7071
+            self.vy *= 0.7071
 
-        self.hDir = hDir
+    def Move(self):
+        self.rect.x += (self.vx * self.speed)
+        self.rect.y += (self.vy * self.speed)
 
-    def SetVerticalDir(self):
-        vDir = 0
-        if self.moveUp:
-            vDir -= 1
-        if self.moveDown:
-            vDir += 1
-
-        self.vDir = vDir
-
-    def CalcSpeed(self):
-        a = self.hDir
-        b = self.vDir
-
-        hypoteneuse = (a*a + b*b)**0.5
-
-        if hypoteneuse != 0:
-            self.speed = 1/hypoteneuse
-        else:
-            self.speed = 0
-        
-    # Move player
-    def MovePlayer(self):
-        cx = self.rect.x
-        cy = self.rect.y
-
-        cx += self.hDir * self.maxSpeed * self.speed
-        cy += self.vDir * self.maxSpeed * self.speed
-
-        self.rect.x = cx
-        self.rect.y = cy
-        
     # Update sprite logic
     def update(self):
-        self.GetInput()
-        self.SetHorizontalDir()
-        self.SetVerticalDir()
-        self.CalcSpeed()
-        self.MovePlayer()
+        self.SetVelocity()
+        self.Move()
         self.delete()
 
     # Destroy sprite

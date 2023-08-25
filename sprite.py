0893be4421830927ec9fa6b976cc12fe9fd54ba8
David Ward <davidmward@mail.com>
2023-08-24 18:29:28 -0700

    update sprite.py template code

--- sprite.py
+++ sprite.py
@@ -2,20 +2,23 @@ import pygame
 
 # class inherets from pygame sprite class
 class Sprite(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, screenWidth, screenHeight):
 
         # Initialize parent sprite class
         super().__init__()
+
+        self.screenWidth = screenWidth
+        self.screenHeight = screenHeight
 
         self.image = None
         self.rect = None
         
     # Update sprite logic
-    def Update(self):
+    def update(self):
         self.destroy()
 
     # Destroy sprite
-    def Destroy(self):
+    def delete(self):
         condition = False
         if condition:
             self.kill()

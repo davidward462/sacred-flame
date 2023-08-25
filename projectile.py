be920bb506332245e4686c5314d86d2dd0b682fa
David Ward <davidmward@mail.com>
2023-08-24 18:34:19 -0700

    add class for projectile

---
+++ projectile.py
@@ -0,0 +1,27 @@
+import pygame
+
+# class inherets from pygame sprite class
+class Projectile(pygame.sprite.Sprite):
+    def __init__(self, screenWidth, screenHeight):
+
+        # Initialize parent sprite class
+        super().__init__()
+
+        self.screenWidth = screenWidth
+        self.screenHeight = screenHeight
+
+        self.image = None
+        self.rect = None
+        
+    # Update sprite logic
+    def update(self):
+        self.destroy()
+
+    # Destroy sprite
+    def delete(self):
+        condition = False
+        if condition:
+            self.kill()
+
+
+

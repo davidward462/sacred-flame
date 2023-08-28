e9a2eba85e0f2b0593cf63ab9dcbeb57433d914f
David Ward <davidmward@mail.com>
2023-08-27 20:50:31 -0700

    keep projectile inside screen

--- projectile.py
+++ projectile.py
@@ -15,15 +15,32 @@ class Projectile(pygame.sprite.Sprite):
         self.image = pygame.image.load('graphics/projectile-temp.png')
         self.rect = self.image.get_rect()
         self.rect.center = (self.startX, self.startY)
-        
+
+        self.insideScreen = True
+
+        self.vx = 0
+        self.vy = 0
+        self.speed = 5
+
+    # Move projectile in particular direction
+    def Move(self):     
+        a = 1
+
+    # Check if projectile is inside the bounds of the screen
+    def IsInsideScreen(self):
+        cx = self.rect.x
+        cy = self.rect.y
+        if cx < 0 or cx > self.screenWidth or cy < 0 or cy > screenHeight:
+            self.insideScreen = False
+
+
     # Update sprite logic
     def update(self):
         self.delete()
 
     # Destroy sprite
     def delete(self):
-        condition = False
-        if condition:
+        if not self.insideScreen:
             self.kill()
 
 

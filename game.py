51cf4ca3b5efbdd232d8527793eb64abd327ce8b
David Ward <davidmward@mail.com>
2023-09-04 19:45:16 -0700

    create game state class

---
+++ game.py
@@ -0,0 +1,23 @@
+import pygame # is this needed?
+
+# Keep track of the current state of the game, and other data
+class Game():
+    def __init__(self):
+        self.currentState = 'title'
+        
+        transitions = {
+                'title': {'spaceInput': 'running'}
+                'running': {'pInput': 'paused', 'playerDeath': 'gameOver', 'success': 'gameWin'}
+        }
+
+    def Transition(self, currentState, event):
+            if currentState in transitions and event in transitions[currentState]:
+                return transitions[currentState, event]
+            else:
+                return currentState
+
+    def Update(self, event):
+        self.currentState = Transition(self.currentState, event)
+
+
+

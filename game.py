85d627647428a6b1d46cdb154f3c09ca9332489a
David Ward <davidmward@mail.com>
2023-09-06 19:31:49 -0700

    comments

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ import pygame # is this needed?
 # Keep track of the current state of the game, and other data
 class Game():
     def __init__(self):
+        # TODO: start on 'title' state
         self.currentState = 'running'
         
         self.transitions = {
@@ -12,14 +13,19 @@ class Game():
         }
 
     def Transition(self, currentState, event):
+            # If there is a transition for the current state, and the event exists for the current state.
             if currentState in self.transitions and event in self.transitions[currentState]:
+                # do trasition
                 return self.transitions[currentState][event]
             else:
+                # else if there is no transition available
                 return currentState
 
+    # Give the game an event and try to perfrom a state transition if possible.
     def Update(self, event):
         self.currentState = self.Transition(self.currentState, event)
 
+    # Return the current state
     def GetState(self):
         return self.currentState
 

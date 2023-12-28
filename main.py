9b560be9045d4045f2b30e44d38527fe21e0a3e1
David Ward <davidmward@mail.com>
2023-12-27 20:29:18 -0800

    lossless scaling of flame with spark pickup

--- main.py
+++ main.py
@@ -326,9 +326,6 @@ def main():
             if event.type == flameTimer and game.IsRunning():
                 flameTimeCurrent -= 1
 
-                # shrink flame
-                flameGroup.sprite.Resize(-1)
-
         # End event loop
 
         # Logical updates
@@ -375,7 +372,6 @@ def main():
             sparkCollision = pygame.sprite.spritecollide(player.sprite, sparkGroup, True)
             if sparkCollision:
                 flameTimeCurrent += 5
-                flameGroup.sprite.Resize(5)
 
                 # keep flame timer below or equal to maximum
                 if flameTimeCurrent > flameTimerMax:
@@ -401,7 +397,7 @@ def main():
         # update scale for flame
         flameFraction = flameTimeCurrent / flameTimerMax
         # flameGroup.sprite.UpdateScale( flameFraction )
-        
+        flameGroup.sprite.SetSize(flameFraction)
 
         # whole number percent
         flamePercent = int(flameFraction * 100)

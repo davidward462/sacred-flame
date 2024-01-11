0823be4b4833b2dbd45cf88dd544fd7170d3bbee
David Ward <davidmward@mail.com>
2024-01-11 00:17:26 -0800

    add title text

--- main.py
+++ main.py
@@ -266,11 +266,17 @@ def main():
     # TODO: determine final font style
     defaultFont = pygame.font.SysFont('freesansbold', 32)
 
+    # Title font
+    titleFont = pygame.font.SysFont('freesansbold', 56)
+    titleText = titleFont.render("Sacred Flame", True, 'white', 'black') 
+    titleTextRect = titleText.get_rect()
+    titleTextRect.center = (currentScreenWidth / 2, (currentScreenHeight / 2) - 100 )
+
     # Start of game font
     # startFont = pygame.font.SysFont('freesansbold', 32)
     startText = defaultFont.render("Press SPACE to begin", True, 'white', 'black') 
     startTextRect = startText.get_rect()
-    startTextRect.center = (currentScreenWidth / 2, currentScreenHeight / 2)
+    startTextRect.center = (currentScreenWidth / 2, (currentScreenHeight / 2) + 100 )
 
     # Pause screen font
     # pauseFont = pygame.font.SysFont('freesansbold', 32)
@@ -494,6 +500,7 @@ def main():
 
         # If on the title screen
         if game.IsTitle():
+            screen.blit(titleText, titleTextRect)
             screen.blit(startText, startTextRect)
 
         # If game is paused
